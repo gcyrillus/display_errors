82e78cb28346e18f86680e44d1781c65e7d2f005
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2023-11-09 09:34:27 +0100

    Update display_error.php

--- display_error.php
+++ display_error.php
@@ -3,85 +3,61 @@
 		* Plugin display_error
 		*
 		* @package	PLX
-		* @version	1.0
-		* @date	22/10/23
+		* @version	1.1
+		* @date	09/11/23
 		* @author gc-nomade
 	**/
 	class display_error extends plxPlugin {
-		private $active;
+		private $active; // etat actif/non actif
 		
 		public function __construct($default_lang) {
 			# appel du constructeur de la classe plxPlugin (obligatoire)
-			parent::__construct($default_lang);
-			
-			
+			parent::__construct($default_lang);				
 			
 			# limite l'acces a l'ecran de configuration du plugin
-			# PROFIL_ADMIN , PROFIL_MANAGER , PROFIL_MODERATOR , PROFIL_EDITOR , PROFIL_WRITER
-			$this->setConfigProfil(PROFIL_ADMIN);
+			$this->setConfigProfil(PROFIL_ADMIN);			
 			
-			
-			$this->active=$this->getParam("active");
-			
-			
+			$this->active=$this->getParam("active");				
 			
 			# Declaration d'un hook (existant ou nouveau)
 			$this->addHook('AdminPrepend','AdminPrepend');
 			$this->addHook('Index','Index');
-			$this->addHook('displayErrors','displayErrors');
-			
+			$this->addHook('displayErrors','displayErrors');			
 			
 		}
 		
-		# Activation / desactivation
-		public function OnActivate() {
-			# code à executer à l’activation du plugin
-		}
 		public function OnDeactivate() {
 			# code à executer à la désactivation du plugin
 			$this->setParam('active','0','numeric');
-			$this->saveParams();
+			$this->saveParams();			
 		}
 		
 		
-		########################################
-		# HOOKS
-		########################################
-		
-		
-		########################################
-		# AdminPrepend
-		########################################
-		# Description:
+		# Affichage backend
 		public function AdminPrepend(){
 			$this->displayErrors();
-			
+			if($this->getParam('active') == '0') $this->aInfos['title'] = $this->getLang('L_NO').$this->getLang('L_ACTIVE').$this->aInfos['title'];
 		}
 		
-		########################################
-		# Index
-		########################################
-		# Description:
+		# Affichage frontend
 		public function Index(){
-			$this->displayErrors();
-			
+			$this->displayErrors();			
 		}
 		
-		########################################
-		# displayErrors
-		########################################
-		# Description:
+		# tente de mettre à jour la configuration PHP
 		public function displayErrors(){
-			try {
-				ini_set('display_errors',1);
-				ini_set('display_startup_errors',1);
-				error_reporting(E_ALL);
-			}
-			catch (Exception $e) {
-				echo  $e->getMessage();
-				$this->setParam('active','0','numeric');
-				$this->saveParams();
-				echo 'Plugin turned OFF ! Plugin ETEINT !';
-			}			
-		}		
-	}
+			if($this->getParam('active') == '1') {
+				try {
+					ini_set('display_errors',1);
+					ini_set('display_startup_errors',1);
+					error_reporting(E_ALL);
+				}
+				catch (Exception $e) {
+					echo  $e->getMessage();
+					$this->setParam('active','0',numeric);
+					$this->saveParams();
+					echo 'Plugin turned OFF ! Plugin ETEINT !';
+				}			
+			}	
+		}
+	}	
